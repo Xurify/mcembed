d2114dcf1c191de54b20b21784c384ae6a313ca7
Xurify <52548195+Xurify@users.noreply.github.com>
2025-03-23 07:39:27 -0400

    Testing out external rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/m/:id.png",
+        destination: "https://modrinth-embed.vercel.app/:id.png",
+      },
+      {
+        source: "/cf/:id(\\d+).png",
+        destination: "https://curseforge-embed.vercel.app/:id.png",
+      }
+    ];
+  },
 };
 
 export default nextConfig;
